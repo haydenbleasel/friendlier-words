dc8987d2a98c7fdfc23e9b2f8d85142f3226319f
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-03 15:28:30 -0400

    Add adjectives

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import animals from '../lib/animals.json' assert { type: 'json' };
 import space from '../lib/space.json' assert { type: 'json' };
 import food from '../lib/food.json' assert { type: 'json' };
 import mythology from '../lib/mythology.json' assert { type: 'json' };
+import adjectives from '../lib/adjectives.json' assert { type: 'json' };
 
 const categories = [animals, space, food, mythology];
 
 export const friendlyWords = (segments = 2, separator = '-'): string => {
   const words: string[] = [];
 
-  for (let index = 0; index < segments; index += 1) {
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  words.push(adjective);
+
+  for (let index = 1; index < segments; index += 1) {
     const category = categories[Math.floor(Math.random() * categories.length)];
     const word = Math.floor(Math.random() * category.length);
     const newWord = category[word];
