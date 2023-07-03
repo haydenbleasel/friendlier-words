8b6d4018cc1958c3b182ef4c05f637d86c0b9f1b
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-03 15:11:58 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,5 @@
 const friendlyWords = require('../dist/index.js').friendlyWords;
 
-console.log(friendlyWords());
-console.log(friendlyWords());
-console.log(friendlyWords());
-console.log(friendlyWords());
-console.log(friendlyWords());
-console.log(friendlyWords());
-console.log(friendlyWords());
+console.log('Default:', friendlyWords());
+console.log('3 Segments:', friendlyWords(3));
+console.log('Custom separator:', friendlyWords(2, '/'));
