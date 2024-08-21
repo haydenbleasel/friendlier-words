b75c48ec6b746e06c2ff9861e8ceddf975a3183c
Hayden Bleasel <hello@haydenbleasel.com>
2024-08-20 20:44:34 -0400

    Add architecture words

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import adjectives from '../lib/adjectives.json' assert { type: 'json' };
 import animals from '../lib/animals.json' assert { type: 'json' };
+import architecture from '../lib/architecture.json' assert { type: 'json' };
 import food from '../lib/food.json' assert { type: 'json' };
 import music from '../lib/music.json' assert { type: 'json' };
 import mythology from '../lib/mythology.json' assert { type: 'json' };
 import nature from '../lib/nature.json' assert { type: 'json' };
 import space from '../lib/space.json' assert { type: 'json' };
 
-const categories = [animals, space, food, mythology, nature, music];
+const categories = [
+  animals,
+  space,
+  food,
+  mythology,
+  nature,
+  music,
+  architecture,
+];
 
 export const friendlyWords = (segments = 2, separator = '-'): string => {
   const words: string[] = [];
